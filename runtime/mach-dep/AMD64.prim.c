3258ed13d63e1f9c6c4bd830f52aef8b3f5e9854
ssoss <skyler.soss@gmail.com>
2025-08-11 11:51:38 -0500

    cmake files

---
+++ runtime/mach-dep/AMD64.prim.c
@@ -0,0 +1,20 @@
+
+#define temp        RAX
+#define misc0       RBX     /* callee save */
+#define misc1       RCX     /* callee save */
+#define misc2       RDX     /* callee save */
+#define misc3       R10
+#define misc4       R11
+#define misc5       R12
+#define misc6       R13
+#define stdcont     RSI
+#define stdarg      RBP
+#define stdlink     R8
+#define stdclos     R9
+#define allocptr    RDI
+#define limitptr    R14
+#define storeptr    R15
+#define stackptr    RSP
+#define creturn 	RAX
+
+
